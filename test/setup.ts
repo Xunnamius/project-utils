6d88a6430ab20934e079c9281cfb4f71bd14f9e5
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2021-11-24 04:36:37 -0800

    test(setup): carry over mock function from multiverse

--- test/setup.ts
+++ test/setup.ts
@@ -1,8 +1,47 @@
 import { name as pkgName } from 'package';
 import debugFactory from 'debug';
+import 'jest-extended/all';
 import 'jest-extended';
+
+import type { Promisable } from 'type-fest';
 
 const debug = debugFactory(`${pkgName}:jest-setup`);
 
 debug(`pkgName: "${pkgName}"`);
 debug(`pkgVersion: "N/A"`);
+
+export async function withMockedOutput(
+  fn: (spies: {
+    logSpy: jest.SpyInstance;
+    warnSpy: jest.SpyInstance;
+    errorSpy: jest.SpyInstance;
+    infoSpy: jest.SpyInstance;
+    stdoutSpy: jest.SpyInstance;
+    stdErrSpy: jest.SpyInstance;
+  }) => unknown
+) {
+  const logSpy = jest.spyOn(console, 'log').mockImplementation(() => undefined);
+  const warnSpy = jest.spyOn(console, 'warn').mockImplementation(() => undefined);
+  const errorSpy = jest.spyOn(console, 'error').mockImplementation(() => undefined);
+  const infoSpy = jest.spyOn(console, 'info').mockImplementation(() => undefined);
+  const stdoutSpy = jest.spyOn(process.stdout, 'write').mockImplementation(() => true);
+  const stdErrSpy = jest.spyOn(process.stderr, 'write').mockImplementation(() => true);
+
+  try {
+    await fn({
+      logSpy,
+      warnSpy,
+      errorSpy,
+      infoSpy,
+      stdoutSpy,
+      stdErrSpy
+    });
+  } finally {
+    logSpy.mockRestore();
+    warnSpy.mockRestore();
+    errorSpy.mockRestore();
+    infoSpy.mockRestore();
+    stdoutSpy.mockRestore();
+    stdErrSpy.mockRestore();
+  }
+}
