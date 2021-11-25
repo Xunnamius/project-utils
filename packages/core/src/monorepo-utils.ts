090ef2efeeceecda6b343a99f24a03c17310e224
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2021-11-24 17:14:11 -0800

    feat(packages/core): add getRunContext to monorepo-utils

--- packages/core/src/monorepo-utils.ts
+++ packages/core/src/monorepo-utils.ts
@@ -1,1 +1,18 @@
-export {};
+import { sep } from 'path';
+
+export function getRunContext({ cwd }: { cwd: string } = { cwd: process.cwd() }) {
+  const path = cwd.split(sep).slice(-2);
+  const [parentDir, basename] = path;
+
+  return parentDir != 'packages'
+    ? ({
+        context: 'polyrepo',
+        basename: null,
+        path: null
+      } as const)
+    : ({
+        context: 'monorepo',
+        basename,
+        path: path.join(sep)
+      } as const);
+}
