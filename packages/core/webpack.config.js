81d6428de4f08b5870bd3e3486c71d4b9c4617d6
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2022-02-23 22:44:46 -0800

    build(packages/core): update local webpack config

--- packages/core/webpack.config.js
+++ packages/core/webpack.config.js
@@ -1,8 +1,20 @@
 'use strict';
 
+const pkgName = require('./package.json').name;
+
 module.exports = {
   'cjs-static': (config) => {
     config.entry['import-aliases'] = `${__dirname}/src/import-aliases.ts`;
-    config.entry['monorepo-utils'] = `${__dirname}/src/monorepo-utils.ts`;
+    config.entry['project-utils'] = `${__dirname}/src/project-utils.ts`;
+    config.entry['errors'] = `${__dirname}/src/errors.ts`;
+    config.externals = [
+      ...config.externals,
+      // ? Externalize shared local imports
+      ({ request }, cb) => {
+        if (request.startsWith('./') && request.endsWith('/errors')) {
+          cb(null, `commonjs ${pkgName}/errors`);
+        } else cb();
+      }
+    ];
   }
 };
