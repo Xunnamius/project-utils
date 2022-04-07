f3502729ccdb1eece95e1d6483d2b456e6c72291
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2022-04-07 03:40:07 -0700

    chore(packages/plugin-lint): add TODO comment

--- packages/plugin-lint/src/utils/link-protection.ts
+++ packages/plugin-lint/src/utils/link-protection.ts
@@ -1,4 +1,5 @@
 export function checkForPotentiallyDisabledLinks() {
+  // TODO
   console.log('TODO: check for potentially disabled links');
   console.log('TODO: use ALLOW_DISABLED_LINKS=1 to skip');
 }
