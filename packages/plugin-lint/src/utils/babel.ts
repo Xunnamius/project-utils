729d4076ec156809140abc5812b815c5300acb7b
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2022-04-09 12:52:39 -0700

    chore(packages/plugin-lint): update comment text

--- packages/plugin-lint/src/utils/babel.ts
+++ packages/plugin-lint/src/utils/babel.ts
@@ -3,9 +3,9 @@ import type { ReporterFactory } from './index';
 
 /**
  * Checks for monorepo sub-root package distributables with cross-dependencies
- * that are not listed in said package's package.json file. This type of error
- * **is not normally caught by unit or integration tests**, making it especially
- * pernicious!
+ * that are not listed in said package's package.json file, or that are not
+ * imported using the pkgverse alias. These types of errors **are not normally
+ * caught by unit or integration tests**, making them especially pernicious!
  */
 export async function checkCrossDependencies({
   pkgJson,
@@ -15,6 +15,7 @@ export async function checkCrossDependencies({
   reporterFactory: ReporterFactory;
 }) {
   void pkgJson, reporterFactory;
+  // TODO: Ensure pkgverse alias is used
   // TODO: Check "module" first, then check "default"/isImplicitDefault
 
   // TODO: Ignore if checked export field entry paths do not exist or don't end
