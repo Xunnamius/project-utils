dc42f18fdfe860ca263ed926d6cfa79a74853197
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2024-05-31 03:36:53 -0700

    chore(release): update command structure again

--- release.config.js
+++ release.config.js
@@ -75,8 +75,7 @@ module.exports = {
           [
             '@semantic-release/exec',
             {
-              prepareCmd:
-                'remark -o --use reference-links --use gfm --use frontmatter CHANGELOG.md'
+              prepareCmd: 'NODE_ENV=format npx remark --output --frail CHANGELOG.md'
             }
           ],
           [
