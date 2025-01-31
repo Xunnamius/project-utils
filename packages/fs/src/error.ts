162e3600f4d5aa2db060be1e5204a6f6b0882cc2
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2025-01-31 00:59:10 -0800

    refactor(packages/fs): split error and index into two entry points

---
+++ packages/fs/src/error.ts
@@ -0,0 +1,11 @@
+import { CommonErrorMessage } from 'multiverse+common:error.ts';
+
+export * from 'multiverse+common:error.ts';
+
+/**
+ * A collection of possible error and warning messages.
+ */
+/* istanbul ignore next */
+export const FsErrorMessage = {
+  ...CommonErrorMessage
+};
--- packages/fs/src/index.ts
+++ packages/fs/src/index.ts
@@ -1,1 +1,6 @@
-export {};
+export * from 'universe+fs:system/derive-virtual-gitignore-lines.ts';
+export * from 'universe+fs:system/derive-virtual-prettierignore-lines.ts';
+export * from 'universe+fs:system/is-accessible.ts';
+export * from 'universe+fs:system/read-json.ts';
+export * from 'universe+fs:system/read-jsonc.ts';
+export * from 'universe+fs:system/read-xpackage-json-at-root.ts';
