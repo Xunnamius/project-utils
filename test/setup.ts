ee5934c633984ac4a231bb944801b6555d9215d8
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2025-02-02 09:26:02 -0800

    test(setup): add missing setup file

---
+++ test/setup.ts
@@ -0,0 +1,11 @@
+/**
+ ** This file is automatically imported by Jest, and is responsible for
+ **  bootstrapping the runtime for every test file.
+ */
+
+// ? jest-extended will always come from @-xun/symbiote (i.e. transitively)
+// {@symbiote/notInvalid jest-extended}
+
+// ? https://github.com/jest-community/jest-extended#typescript
+import 'jest-extended';
+import 'jest-extended/all';
