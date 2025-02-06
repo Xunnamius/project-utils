a54ab992a20108f09437f7126a5e7e6248bf5ea5
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2025-02-06 11:47:01 -0800

    test(packages/graph): disable integration test for now

--- packages/graph/test/integration/integration-node-smoke.test.ts
+++ packages/graph/test/integration/integration-node-smoke.test.ts
@@ -9,113 +9,115 @@
 /* eslint-disable unicorn/no-abusive-eslint-disable */
 /* eslint-disable */
 
-import {
-  dummyDirectoriesFixture,
-  dummyFilesFixture,
-  dummyNpmPackageFixture,
-  ensurePackageHasBeenBuilt,
-  mockFixtureFactory,
-  nodeImportAndRunTestFixture,
-  npmLinkPackageFixture,
-  reconfigureJestGlobalsToSkipTestsInThisFileIfRequested
-} from 'testverse:util.ts';
+test.todo('disable this for now');
 
-import { createDebugLogger } from 'rejoinder';
-import { toDirname } from '@-xun/fs';
-import {
-  name as packageName,
-  version as packageVersion,
-  exports as packageExports
-} from 'rootverse+graph:package.json';
+// import {
+//   dummyDirectoriesFixture,
+//   dummyFilesFixture,
+//   dummyNpmPackageFixture,
+//   ensurePackageHasBeenBuilt,
+//   mockFixtureFactory,
+//   nodeImportAndRunTestFixture,
+//   npmLinkPackageFixture,
+//   reconfigureJestGlobalsToSkipTestsInThisFileIfRequested
+// } from 'testverse:util.ts';
 
-reconfigureJestGlobalsToSkipTestsInThisFileIfRequested({ it: true });
+// import { createDebugLogger } from 'rejoinder';
+// import { toDirname } from '@-xun/fs';
+// import {
+//   name as packageName,
+//   version as packageVersion,
+//   exports as packageExports
+// } from 'rootverse+graph:package.json';
 
-const TEST_IDENTIFIER = 'integration-node';
-const debug = createDebugLogger({ namespace: 'graph' }).extend(TEST_IDENTIFIER);
-const nodeVersion = process.env.XPIPE_MATRIX_NODE_VERSION || process.version;
+// reconfigureJestGlobalsToSkipTestsInThisFileIfRequested({ it: true });
 
-debug(`nodeVersion: "${nodeVersion}"`);
+// const TEST_IDENTIFIER = 'integration-node';
+// const debug = createDebugLogger({ namespace: 'graph' }).extend(TEST_IDENTIFIER);
+// const nodeVersion = process.env.XPIPE_MATRIX_NODE_VERSION || process.version;
 
-beforeAll(async () => {
-  ensurePackageHasBeenBuilt(
-    toDirname(require.resolve('rootverse+graph:package.json')),
-    packageName,
-    packageExports
-  );
-});
+// debug(`nodeVersion: "${nodeVersion}"`);
 
-const withMockedFixture = mockFixtureFactory(
-  [
-    dummyNpmPackageFixture,
-    dummyDirectoriesFixture,
-    dummyFilesFixture,
-    npmLinkPackageFixture,
-    nodeImportAndRunTestFixture
-  ],
-  {
-    performCleanup: true,
-    identifier: TEST_IDENTIFIER,
-    directoryPaths: ['packages/pkg1', 'packages/pkg2', '.git'],
-    initialVirtualFiles: {
-      'package.json': /* js */ `{
-        "name":"dummy-pkg",
-        "workspaces": ["packages/*"],
-        "dependencies": {
-          "${packageName}": "${packageVersion}"
-        }
-      }`,
-      'packages/pkg1/package.json': /* js */ `{
-        "name":"pkg-1",
-        "version":"1.2.3"
-      }`,
-      'packages/pkg2/package.json': /* js */ `{
-        "name":"pkg-2",
-        "version":"1.2.3"
-      }`
-    }
-  }
-);
+// beforeAll(async () => {
+//   ensurePackageHasBeenBuilt(
+//     toDirname(require.resolve('rootverse+graph:package.json')),
+//     packageName,
+//     packageExports
+//   );
+// });
 
-const runTest = async (
-  importAs: 'esm' | 'cjs',
-  testFixtureFn: Parameters<typeof withMockedFixture>[0]
-) => {
-  await withMockedFixture(
-    async (context) => {
-      await testFixtureFn(context);
-    },
-    {
-      initialVirtualFiles: {
-        [`src/index.${importAs === 'esm' ? 'm' : ''}js`]:
-          (importAs === 'esm'
-            ? /* ts */ `import { analyzeProjectStructure } from '${packageName}/project-utils';`
-            : /* ts */ `const { analyzeProjectStructure } = require('${packageName}/project-utils');`) +
-          '\n' +
-          (importAs === 'esm'
-            ? /* ts */ `import { getEslintAliases } from '${packageName}/???';`
-            : /* ts */ `const { getEslintAliases } = require('${packageName}/???');`) +
-          /* ts */ `
-console.log(analyzeProjectStructure().project.json.name === 'dummy-pkg');
-console.log(analyzeProjectStructure().project.packages.get('pkg-1').json.name === 'pkg-1');
-console.log(getEslintAliases()[0][0] === 'universe' && getEslintAliases()[0][1] === './src');
-`
-      }
-    }
-  );
-};
+// const withMockedFixture = mockFixtureFactory(
+//   [
+//     dummyNpmPackageFixture,
+//     dummyDirectoriesFixture,
+//     dummyFilesFixture,
+//     npmLinkPackageFixture,
+//     nodeImportAndRunTestFixture
+//   ],
+//   {
+//     performCleanup: true,
+//     identifier: TEST_IDENTIFIER,
+//     directoryPaths: ['packages/pkg1', 'packages/pkg2', '.git'],
+//     initialVirtualFiles: {
+//       'package.json': /* js */ `{
+//         "name":"dummy-pkg",
+//         "workspaces": ["packages/*"],
+//         "dependencies": {
+//           "${packageName}": "${packageVersion}"
+//         }
+//       }`,
+//       'packages/pkg1/package.json': /* js */ `{
+//         "name":"pkg-1",
+//         "version":"1.2.3"
+//       }`,
+//       'packages/pkg2/package.json': /* js */ `{
+//         "name":"pkg-2",
+//         "version":"1.2.3"
+//       }`
+//     }
+//   }
+// );
 
-it('works as an ESM import', async () => {
-  expect.hasAssertions();
-  await runTest('esm', async (context) => {
-    expect(context.testResult.stdout).toBe('true\ntrue\ntrue');
-    expect(context.testResult.exitCode).toBe(0);
-  });
-});
+// const runTest = async (
+//   importAs: 'esm' | 'cjs',
+//   testFixtureFn: Parameters<typeof withMockedFixture>[0]
+// ) => {
+//   await withMockedFixture(
+//     async (context) => {
+//       await testFixtureFn(context);
+//     },
+//     {
+//       initialVirtualFiles: {
+//         [`src/index.${importAs === 'esm' ? 'm' : ''}js`]:
+//           (importAs === 'esm'
+//             ? /* ts */ `import { analyzeProjectStructure } from '${packageName}/project-utils';`
+//             : /* ts */ `const { analyzeProjectStructure } = require('${packageName}/project-utils');`) +
+//           '\n' +
+//           (importAs === 'esm'
+//             ? /* ts */ `import { getEslintAliases } from '${packageName}/???';`
+//             : /* ts */ `const { getEslintAliases } = require('${packageName}/???');`) +
+//           /* ts */ `
+// console.log(analyzeProjectStructure().project.json.name === 'dummy-pkg');
+// console.log(analyzeProjectStructure().project.packages.get('pkg-1').json.name === 'pkg-1');
+// console.log(getEslintAliases()[0][0] === 'universe' && getEslintAliases()[0][1] === './src');
+// `
+//       }
+//     }
+//   );
+// };
 
-it('works as a CJS require(...)', async () => {
-  expect.hasAssertions();
-  await runTest('cjs', async (context) => {
-    expect(context.testResult.stdout).toBe('true\ntrue\ntrue');
-    expect(context.testResult.exitCode).toBe(0);
-  });
-});
+// it('works as an ESM import', async () => {
+//   expect.hasAssertions();
+//   await runTest('esm', async (context) => {
+//     expect(context.testResult.stdout).toBe('true\ntrue\ntrue');
+//     expect(context.testResult.exitCode).toBe(0);
+//   });
+// });
+
+// it('works as a CJS require(...)', async () => {
+//   expect.hasAssertions();
+//   await runTest('cjs', async (context) => {
+//     expect(context.testResult.stdout).toBe('true\ntrue\ntrue');
+//     expect(context.testResult.exitCode).toBe(0);
+//   });
+// });
