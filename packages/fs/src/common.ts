c387a46cf313de4a08785b452b242fe7fa0c6d57
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2025-02-23 20:07:22 -0800

    chore(packages/fs): use proper debug namespace

--- packages/fs/src/common.ts
+++ packages/fs/src/common.ts
@@ -3,5 +3,5 @@ import { createDebugLogger } from 'rejoinder';
 import { globalDebuggerNamespace } from 'multiverse+common:constant.ts';
 
 export const commonDebug = createDebugLogger({
-  namespace: `${globalDebuggerNamespace}:analyze`
+  namespace: `${globalDebuggerNamespace}:fs`
 });
